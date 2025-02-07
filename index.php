a938fea688dff2a392207a47c2d539db547b3936
Regular001(1053) <kabriacid01@gmail.com>
2025-02-07 19:28:35 +0100

    commit

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 session_start();
 require __DIR__ . '/config/config.php';
-require __DIR__ . '/public/partials/header.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AAS Data Sub - Home</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+
 <body>
-    <?php require __DIR__ . '/public/partials/navbar.php'; ?>
+
 </body>
